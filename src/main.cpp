d415b4ba9e0c408aca99a8ad8e32b1f8f44c4381
Taras Shypytiak <taras.shypytiak@artcom-venture.de>
2022-10-13 22:01:58 +0300

    calbacks

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,53 @@
 
 #include <iostream>
 
-int main(void)
-{
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void onWindowSizeChangedStatic(GLFWwindow* window, int width, int height){
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void onKeyPressed(GLFWwindow* pWnd, int keyCode, int scanCode, int action, int mode) {
+    if (keyCode == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(pWnd, GL_TRUE);
+    }
+}
+
+int main(void){
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 6 );
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(window, onWindowSizeChangedStatic);
+    glfwSetKeyCallback(window, onKeyPressed);
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
     if( !gladLoadGL() ){
-        std::cout << "Faile to load glad" << std::endl;
+        std::cout << "Failed to load glad" << std::endl;
         return -1;
     }
 
     std::cout << "Opengl " <<  GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renerer: " << glGetString(GL_RENDERER) << std::endl;
     glClearColor( 0, 1, 0, 1 );
 
     /* Loop until the user closes the window */
